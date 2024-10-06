628906e823aa950eebe12485de50120483f833da
D4-CTM <josuedelcid325@gmail.com>
2024-10-05 21:37:37 -0600

    player turn selected

--- JScript.js
+++ JScript.js
@@ -2,9 +2,21 @@ let computerWinCount = 0;
 let playerWinCount = 0;
 const maxRounds = 5;
 
+/*
+* 
+*/
+function getPlayerTurn(turn) {
+    return +prompt(`Turno #${turn}\nIngrese una de las siguientes opciones:\n1. Piedra\n2. Papel\n3. Tijera`);    
+}
+
 const main = (val) => {
     if (val < maxRounds) 
     {
+        do {
+            playerAns = getPlayerTurn(val + 1);
+        } while (playerAns < 1 || playerAns > 3)
+        console.log(val);
+        main(val + 1);
     }
 }
 
