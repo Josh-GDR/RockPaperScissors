cebf7c80827a2aa96e69f851d49447a6065aa470
D4-CTM <josuedelcid325@gmail.com>
2024-10-05 21:45:57 -0600

    Computer turn --done

--- JScript.js
+++ JScript.js
@@ -2,20 +2,22 @@ let computerWinCount = 0;
 let playerWinCount = 0;
 const maxRounds = 5;
 
-/*
-* 
-*/
-function getPlayerTurn(turn) {
-    return +prompt(`Turno #${turn}\nIngrese una de las siguientes opciones:\n1. Piedra\n2. Papel\n3. Tijera`);    
+function getPlayerInput(turn) {
+    return +prompt(`Turno #${turn}\nIngrese una de las siguientes opciones:\n1. Piedra\n2. Papel\n3. Tijera`);
+}
+
+function getComputerInput() {
+    return Math.floor(Math.random() * 100)%3;
 }
 
 const main = (val) => {
     if (val < maxRounds) 
     {
         do {
-            playerAns = getPlayerTurn(val + 1);
+            playerAns = getPlayerInput(val + 1);
         } while (playerAns < 1 || playerAns > 3)
-        console.log(val);
+        computerAns = getComputerInput();
+        console.log(computerAns);
         main(val + 1);
     }
 }
