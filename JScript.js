5ff8c420be988014590542bbe1f226528fcfc544
D4-CTM <josuedelcid325@gmail.com>
2024-10-05 22:07:44 -0600

    game finished

--- JScript.js
+++ JScript.js
@@ -2,12 +2,43 @@ let computerWinCount = 0;
 let playerWinCount = 0;
 const maxRounds = 5;
 
+function getBasicInfo(turn) {
+    return `Turn # ${turn}\nMatches won by:\nPlayer: ${playerWinCount}   Computer: ${computerWinCount}`;
+}
+
 function getPlayerInput(turn) {
-    return +prompt(`Turno #${turn}\nIngrese una de las siguientes opciones:\n1. Piedra\n2. Papel\n3. Tijera`);
+    return +prompt(getBasicInfo(turn) + "\nChoose one of the next options (1-3):\n1. Rock\n2. Paper\n3. Scissor");
 }
 
 function getComputerInput() {
     return Math.floor(Math.random() * 100)%3;
+}
+
+function playMatch(playerTurn, computerTurn) {
+    if (playerTurn == computerTurn) {
+        return "It was a draw"
+    }
+
+    if (playerTurn == 3 && computerTurn == 1) {
+        playerWinCount++;
+        return "Player won";
+    }
+
+    if (computerTurn == 3 && playerTurn == 1) {
+        computerWinCount++;
+        return "Computer won";
+    }
+
+    if (playerTurn < computerTurn) {
+        playerWinCount++;
+        return "Player won";
+    }
+
+    if (computerTurn < playerTurn) {
+        computerWinCount++;
+        return "Computer won";
+    }
+
 }
 
 const main = (val) => {
@@ -17,8 +48,12 @@ const main = (val) => {
             playerAns = getPlayerInput(val + 1);
         } while (playerAns < 1 || playerAns > 3)
         computerAns = getComputerInput();
-        console.log(computerAns);
+        alert(playMatch(playerAns, computerAns));
         main(val + 1);
+    }
+
+    if (val == 0) {
+        alert(getBasicInfo("-- game ended --") + `   Draws: ${maxRounds - (computerWinCount + playerWinCount)}`);
     }
 }
 
