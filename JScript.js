ca666ef455c6a15d61f6a8ead56fd0fbbd1d5c96
D4-CTM <josuedelcid325@gmail.com>
2024-10-06 15:17:35 -0600

    result div finish

--- JScript.js
+++ JScript.js
@@ -3,14 +3,40 @@ let playerWinCount = 0;
 let playerChoice = 0;
 const maxRounds = 5;
 
+const playerInfo = document.createElement("p");
+const computerInfo = document.createElement("p");
+const result = document.createElement("p");
+
 const buttons = document.querySelectorAll(".choice");
 buttons.forEach(button => {
     button.addEventListener(("click"), () => {
         playerChoice = +button.id
 
         playMatch(playerChoice, getComputerInput());
+
     })
 })
+
+function initResultDiv() {
+    const content = document.querySelector("#results");
+
+    const headerInfo = document.createElement("h2");
+    headerInfo.textContent = "Matches won by: ";
+    headerInfo.style.cssText = "margin-bottom: 5px"
+    content.appendChild(headerInfo);
+
+    playerInfo.style.cssText = "margin-bottom: 0px"
+    playerInfo.textContent = getPlaterInfo();
+    content.appendChild(playerInfo);
+
+    computerInfo.style.cssText = "margin-bottom: 0px"
+    computerInfo.textContent = getComputerInfo();
+    content.appendChild(computerInfo);
+
+    result.textContent = "-- waiting to start game --";
+    content.appendChild(result);
+}
+initResultDiv();
 
 function getComputerInput() {
     return Math.floor(Math.random() * 100)%3;
@@ -43,10 +69,10 @@ function playMatch(playerTurn, computerTurn) {
 
 }
 
-function getBasicInfo(turn) {
-    return `Turn # ${turn}\nMatches won by:\nPlayer: ${playerWinCount}   Computer: ${computerWinCount}`;
+function getPlaterInfo() {
+    return `Player: ${playerWinCount}`
 }
 
-function getPlayerInput(turn) {
-    return +prompt(getBasicInfo(turn) + "\nChoose one of the next options (1-3):\n1. Rock\n2. Paper\n3. Scissor");
+function getComputerInfo() {
+    return `Computer: ${computerWinCount}`;
 }
